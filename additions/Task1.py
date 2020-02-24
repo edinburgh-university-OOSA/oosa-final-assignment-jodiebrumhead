ab76f59db295e3a453e85c09a6dc8f3f1fd2974e
Jodie Brumhead <s1891967@burn.geos.ed.ac.uk>
2020-02-24 23:54:00 +0000

    task1 final edits

--- additions/Task1.py
+++ additions/Task1.py
@@ -1,13 +1,21 @@
+"""
+
+"""
+
 import argparse
 import numpy as np
+import resource
+from timeit import default_timer as timer
 import sys
 sys.path.append('../src')
-import resource
+
 
 
 from processLVIS import *
 from handleTiff import *
 from filename import *
+from bytesprint import *
+
 
 """Class to process and individual h5 lvis file to find ground level of each wave"""
 
@@ -21,8 +29,9 @@ class indfile():
             # set some bounds
             x0=b.bounds[0]
             y0=b.bounds[1]
-            x1=(b.bounds[2]-b.bounds[0])/15+b.bounds[0]
-            y1=(b.bounds[3]-b.bounds[1])/15+b.bounds[1]
+            x1=(b.bounds[2]-b.bounds[0])/10+b.bounds[0]
+            y1=(b.bounds[3]-b.bounds[1])/10+b.bounds[1]
+
             # read in bounds
             self.c=lvisGround(filename,minX=x0,minY=y0,maxX=x1,maxY=y1)
 
@@ -38,6 +47,7 @@ class indfile():
 
 
 
+
 class packArray():
     #data = lvis.zG
     #lvis.lat
@@ -49,33 +59,37 @@ class packArray():
 
         '''
         self.res = res
-        print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+        #print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
         # determine bounds
         self.minX=np.min(x)
         maxX=np.max(x)
         minY=np.min(y)
         self.maxY=np.max(y)
 
-        print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+        #print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
 
         # determine image size
         self.nX=int((maxX-self.minX)/self.res+1)
         self.nY=int((self.maxY-minY)/self.res+1)
-        print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
-        # pack in to array
-        self.imageArr=np.full((self.nY,self.nX),-999)        # make an array of missing data flags
-        xInds=np.array((x-self.minX)/self.res,dtype=int)  # determine which pixels the data lies in
-        yInds=np.array((self.maxY-y)/self.res,dtype=int)  # determine which pixels the data lies in
 
-        print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+
+        # pack in to array
+        self.imageArr=np.full((self.nY,self.nX),-999, dtype=np.int16)
+        # make an array of missing data flags
+        # dtype=np.int16 reduces data usage - cant use unsigned due to negative vals over sea
+        print(f'Image array bytes: {ps(self.imageArr.nbytes)}')
+
+
+        self.xInds=np.array((x-self.minX)/self.res,dtype=int)  # determine which pixels the data lies in
+        #print(self.xInds.nbytes)
+        self.yInds=np.array((self.maxY-y)/self.res,dtype=int)  # determine which pixels the data lies in
+        #print(self.yInds.nbytes)
+
+
         # this is a simple pack which will assign a single footprint to each pixel try to change this to find average?
 
-        self.imageArr[yInds,xInds]=data
-        print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
-
-class interpolate():
-    pass
-
+        self.imageArr[self.yInds,self.xInds]=data
+        print(f'Image array bytes: {ps(self.imageArr.nbytes)}')
 
 
 
@@ -91,7 +105,7 @@ def ap():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-f', '--fileinput',
-    default='/geos/netdata/avtrain/data/3d/oosa/assignment/lvis/2015/ILVIS1B_AQ2015_1017_R1605_043439.h5',
+    default='/geos/netdata/avtrain/data/3d/oosa/assignment/lvis/2015/ILVIS1B_AQ2015_1017_R1605_061585.h5',
     help='Input path and file you wish to process')
 
     parser.add_argument('-r', '--resolution', default=10, type=int, help='Enter resolution(m) for output DEM (Default = 10m)')
@@ -105,31 +119,35 @@ def ap():
     return parser.parse_args()
 
 
-if __name__ == "__main__":
-    '''Main block'''
+if __name__ == "__main__": # only run below if script called directly
+    """Main Block"""
 
+    start = timer()
+
+    """Unpack argument parser"""
     args = ap()
-
     infilename = args.fileinput
     res = args.resolution
     testing = args.testing
     inEPSG = args.inputEPSG
     outEPSG = args.outputEPSG
 
-    #inEPSG = 4326 # is it self describing data ?
-    #outEPSG = 3031
-
-    f=filenameregex()
-    f.findfilename(infilename)
-    f.finddeets()
-
-    outfilename = f.file + ".tif"  # make this take file name from input filename
+    """Create output filename"""
+    f=filenameregex()  # initialiser
+    f.findfilename(infilename)  # call method to extract filename from path and .h5 extension
+    outfilename = '/scratch/s1891967/' + f.file + ".tif"  # create out filename string
 
 
     a=indfile(infilename, testing, inEPSG, outEPSG)
     print('indfile done')
-    aa=packArray(a.c.zG, a.c.lat, a.c.lon, res)
-    print('arraypacked')
-    aaa=tiffHandle(outfilename)
 
+    aa=packArray(a.c.zG, a.c.lon, a.c.lat, res)
+    print('arraypacked')
+
+    aaa=tiffHandle(outfilename)
     tiffHandle.writeTiff(aa,outfilename,outEPSG)
+
+
+    print(f'RAM usage: {ps((resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)*1000)}')
+    end = timer()
+    print(f'Time taken = {end-start}')
